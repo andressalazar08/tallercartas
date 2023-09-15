03bef61af04d2d7e0575d65f7ebe4d30c3583d8f
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-09-15 12:28:44 -0500

    imagenes cartas y clase carta

--- JuegoCartas/src/main/java/juegocartas/Carta.java
+++ JuegoCartas/src/main/java/juegocartas/Carta.java
@@ -7,10 +7,17 @@ package juegocartas;
  */
 
 import java.util.Random;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 public class Carta {
     
     private int indice;
+    
+    public int getIndice(){
+       return indice;
+   }
     
     public Carta(Random r){
         indice=r.nextInt(52)+1;//aleatoriamente asigna un numero
@@ -30,4 +37,30 @@ public class Carta {
             return Pinta.DIAMANTE;
         }
     }
+    
+    //DEVOLVER EL NOMBRE DE LA CARTA
+    public NombreCarta getNombre(){
+        int numero= indice % 13;
+        if(numero==0){
+            numero=13;
+        }
+        //trabajo con el vector
+        return NombreCarta.values()[numero-1];
+    }
+    
+    //mostrar en el panel
+    public void mostrar(JPanel pnl, int x, int y ){
+        String nombreImagen="/imagenes/carta"+String.valueOf(indice)+".jpg";
+        
+   
+        
+        ImageIcon imagen = new ImageIcon(getClass().getResource(nombreImagen));
+        JLabel lbl = new JLabel(imagen);
+        lbl.setBounds(x, y, imagen.getIconWidth(), imagen.getIconHeight());
+
+        pnl.add(lbl);
+    }
+    
+    
+    
 }
