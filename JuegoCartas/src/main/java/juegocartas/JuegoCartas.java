399ebf115733254890ca814380af7d15c7d87731
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2023-09-15 11:20:34 -0500

    creacion de form y clase jugador

---
+++ JuegoCartas/src/main/java/juegocartas/JuegoCartas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package juegocartas;
+
+/**
+ *
+ * @author afyc2
+ */
+public class JuegoCartas {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+           new FrmJuego().setVisible(true);
+    }
+}
